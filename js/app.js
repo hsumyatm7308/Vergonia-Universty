e342019f16d77e3fd6d4ea5249875ac018405b11
EXY <107622230+hsumyatm7308@users.noreply.github.com>
2022-09-16 09:26:24 +0630

    15/9/22

--- js/app.js
+++ js/app.js
@@ -47,7 +47,40 @@ getcountervalues.forEach(function(getcountervalue) {
 
 // end student counter section 
 
+// Start Rating Section
 
-// End Javascript Area
+// start google code for chart
+google.charts.load("current", { packages: ["corechart"] });
+google.charts.setOnLoadCallback(drawChart);
 
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+    ["Task", "Hours per Day"],
+    ["Myanmar", 8],
+    ["Thailand", 2],
+    ["Signapore", 4],
+    ["Indonesia", 2],
+    ["Srilanka", 8],
+  ]);
+
+  var options = {
+    title: "International Students",
+    // is3D: true,
+    // pieHole: 0.2,
+    width: 550,
+    height: 400,
+  };
+
+  var chart = new google.visualization.PieChart(
+    document.getElementById("piechart")
+  );
+
+  chart.draw(data, options);
+}
+
+// end google code for chart
+// End Rating Section
+
+
+//End Javascript area
 // 25CT
